f109da9f1f07c98078c5dc97545e1008c31db3b4
Gabriel2980 <gabrielvertamatti@gmail.com>
2025-04-30 12:30:52 -0300

    construtores das estruturas

--- ProjetoEstDados.c
+++ ProjetoEstDados.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define LEN 20
-
-typedef struct Registro{
-  char nome[50];
-  int idade;
-  char rg[10];
-  Data entrada;
-}Registro;
 
 typedef struct Data{
   int dia;
@@ -16,15 +10,27 @@ typedef struct Data{
   int ano;
 }Data;
 
-typedef struct Lista{
-    int qtde;
-    ELista *inicio;
-}Lista;
+typedef struct Registro{
+  char nome[50];
+  int idade;
+  char rg[10];
+  Data *entrada;
+}Registro;
 
 typedef struct ELista{
     Registro *dados;
     ELista *proximo;
 }ELista;
+
+typedef struct Lista{
+    int qtde;
+    ELista *inicio;
+}Lista;
+
+typedef struct EFila{
+    EFila *proximo;
+    Registro *dados;
+}EFila;
 
 typedef struct Fila{
     int qtde;
@@ -32,12 +38,89 @@ typedef struct Fila{
     EFila *tail;
 }Fila;
 
-typedef struct EFila{
-    EFila *proximo;
-    Registro dados;
-}EFila;
-
 typedef struct Heap{
-  Registro dados[LEN];
+  Registro *dados[LEN];
   int qtde;
 }Heap;
+
+typedef struct EABB{
+  Registro *dados;
+  EABB *filho_esq;
+  EABB *filho_dir;
+}EABB;
+
+typedef struct ABB{
+  EABB *raiz;
+  int qtde;
+}ABB;
+
+Data *cria_data(int dia, int mes, int ano){
+  Data *data = malloc(sizeof(Data));
+  data->dia = dia;
+  data->mes = mes;
+  data->ano = ano;
+  return data;
+}
+
+Registro *cadastrar(char nome[50], int idade, char rg[10], Data *entrada){
+  Registro *registro = malloc(sizeof(Registro));
+  strcpy(registro->nome, nome);
+  registro->idade = idade;
+  strcpy(registro->rg, rg);
+  registro->entrada = entrada;
+  return registro;
+}
+
+ELista *cria_elista(Registro *registro){
+  ELista *elista = malloc(sizeof(ELista));
+  elista->dados = registro;
+  elista->proximo = NULL;
+  return elista;
+}
+
+Lista *cria_lista(){
+  Lista *lista = malloc(sizeof(Lista));
+  lista->inicio = NULL;
+  lista->qtde = 0;
+  return lista;
+}
+
+EFila *cria_efila(Registro *dados){
+  EFila *efila = malloc(sizeof(EFila));
+  efila->dados = dados;
+  efila->proximo = NULL;
+  return efila;
+}
+
+Fila *cria_fila(){
+  Fila *fila = malloc(sizeof(Fila));
+  fila->head = NULL;
+  fila->head = NULL;
+  fila->qtde = 0;
+}
+
+Heap *cria_heap(){
+  Heap *heap = malloc(sizeof(Heap));
+  int i;
+  for(i = 0; i < LEN; i++){
+    heap->dados[i] = NULL;
+  }
+  heap->qtde = 0;
+}
+
+EABB *cria_eabb(Registro *dados){
+  EABB *eabb = malloc(sizeof(EABB));
+  eabb->dados = dados;
+  eabb->filho_esq = NULL;
+  eabb->filho_dir = NULL;
+}
+
+ABB *cria_abb(){
+  ABB *abb = malloc(sizeof(ABB));
+  abb->raiz = NULL;
+  abb->qtde = 0;
+}
+
+int main(){
+
+}
